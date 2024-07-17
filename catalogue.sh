0497150e6a15bcaf9408d987495d681bd51d6fd7
renuka <hello@123.com>
2024-07-17 08:55:22 +0530

    roboshop frontend script

--- catalogue.sh
+++ catalogue.sh
@@ -17,5 +17,5 @@ systemctl enable catalogue
 systemctl start catalogue
 
 dnf install mongodb-mongosh -y
-mongosh --host localhost</app/db/master-data.js
+mongosh --host mongodb.dev.renuka.online</app/db/master-data.js
 
