bab66d952a87a631e2b8039d5fa50e789acc8e67
renuka <hello@123.com>
2024-07-17 14:34:36 +0530

    roboshop frontend script

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,6 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
