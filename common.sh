22aff8c60a32959c8da2363060310ab39cdd0617
renuka <hello@123.com>
2024-07-20 19:35:19 +0530

    keeping code dry

--- common.sh
+++ common.sh
@@ -1,20 +1,38 @@
 NODEJS () {
+
+  echo disable nodejs default version
   dnf module disable nodejs -y
+
+  echo enable nojejs 20 module
   dnf module enable nodejs:20 -y
+
+  echo install nodejs
   dnf install nodejs -y
 
+  echo copy service file
   cp ${component}.service /etc/systemd/system/${component}.service
+
+  echo copy mongodb repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+  echo adding application user
   useradd roboshop
 
+  echo cleaning old content
   rm -rf /app
+
+  echo create app directory
   mkdir /app
+
+  echo download app content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
   cd /app
+
+  echo extract app content
   unzip /tmp/${component}.zip
 
 
-  cd /app
+  echo install nodejs dependcies
   npm install
   systemctl daemon-reload
   systemctl enable ${component}
