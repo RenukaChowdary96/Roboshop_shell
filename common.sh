3072dffa33e2e07217a12886bd6efb112e46d88f
renuka <hello@123.com>
2024-07-22 16:57:54 +0530

    keeping code dry

--- common.sh
+++ common.sh
@@ -1,40 +1,41 @@
+LOG_FILE=/tmp/Roboshop.log
 NODEJS () {
 
   echo disable nodejs default version
-  dnf module disable nodejs -y
+  dnf module disable nodejs -y >LOG_FILE
 
   echo enable nojejs 20 module
-  dnf module enable nodejs:20 -y
+  dnf module enable nodejs:20 -y >LOG_FILE
 
   echo install nodejs
-  dnf install nodejs -y
+  dnf install nodejs -y >LOG_FILE
 
   echo copy service file
-  cp ${component}.service /etc/systemd/system/${component}.service
+  cp ${component}.service /etc/systemd/system/${component}.service >LOG_FILE
 
   echo copy mongodb repo file
-  cp mongo.repo /etc/yum.repos.d/mongo.repo
+  cp mongo.repo /etc/yum.repos.d/mongo.repo >LOG_FILE
 
   echo adding application user
-  useradd roboshop
+  useradd roboshop >LOG_FILE
 
   echo cleaning old content
-  rm -rf /app
+  rm -rf /app >LOG_FILE
 
   echo create app directory
-  mkdir /app
+  mkdir /app >LOG_FILE
 
   echo download app content
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip >LOG_FILE
   cd /app
 
   echo extract app content
-  unzip /tmp/${component}.zip
+  unzip /tmp/${component}.zip >LOG_FILE
 
 
   echo install nodejs dependcies
-  npm install
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  npm install >/tmp/Roboshop.log
+  systemctl daemon-reload >LOG_FILE
+  systemctl enable ${component}>LOG_FILE
+  systemctl restart ${component}>LOG_FILE
 }
