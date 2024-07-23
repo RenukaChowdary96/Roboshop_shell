c18673d7ddf94ba14287e7b0657c2cb84c7784eb
renuka <hello@123.com>
2024-07-23 23:10:17 +0530

    keeping code dry

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,27 @@
 source common.sh
 component=frontend
-app_path=/usr/share/nginx/html/
+app_path=/usr/share/nginx/html
 
-PRINT Disable  Nginx Default Version
-dnf module disable nginx -y &>>$LOG_FILE
+
+PRINT Disable Nginx default Version
+dnf module disable nginx -y  &>>$LOG_FILE
 STAT $?
 
-PRINT Enable nginx default version
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
+PRINT Enable Nginx 24 Version
+dnf module enable nginx:1.24 -y  &>>$LOG_FILE
 STAT $?
 
-PRINT install Nginx
-dnf install nginx -y &>>$LOG_FILE
+PRINT Install Nginx
+dnf install nginx -y  &>>$LOG_FILE
 STAT $?
 
-PRINT copy nginx config file
-cp nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+PRINT Copy nginx config file
+cp nginx.conf /etc/nginx/nginx.conf  &>>$LOG_FILE
 STAT $?
 
 APP_PREREQUISTES
 
-PRINT Start  Nginx service
-systemctl enable nginx &>>$LOG_FILE
-systemctl restart nginx &>>$LOG_FILE
+PRINT Start Nginx Service
+systemctl enable nginx  &>>$LOG_FILE
+systemctl restart nginx  &>>$LOG_FILE
 STAT $?
