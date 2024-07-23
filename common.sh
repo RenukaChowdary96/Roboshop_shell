80fdb29902435870d9720d53df15908e779a8582
renuka <hello@123.com>
2024-07-23 19:47:46 +0530

    keeping code dry

--- common.sh
+++ common.sh
@@ -13,6 +13,8 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "Refer the log file for the more information: file path: ${LOG_FILE}"
     exit $1
   fi
 
