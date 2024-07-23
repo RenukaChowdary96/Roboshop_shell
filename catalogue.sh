4f7b6846a2ef88daed2492f11b729b5d4876af0e
renuka <hello@123.com>
2024-07-23 19:02:17 +0530

    keeping code dry

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,9 @@ NODEJS
 
 echo install mongodb cilent
 dnf install mongodb-mongosh -y &>>$LOG_FILE
+echo $?
 
 echo load master data
 mongosh --host mongo.dev.renuka.online </app/db/master-data.js &>>$LOG_FILE
+echo $?
 
--- common.sh
+++ common.sh
@@ -11,39 +11,51 @@ NODEJS () {
 
   PRINT disable nodejs default version
   dnf module disable nodejs -y &>>$LOG_FILE
+  echo $?
 
     PRINT  enable nojejs 20 module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
+  echo $?
 
     PRINT  install nodejs
   dnf install nodejs -y &>>$LOG_FILE
+  echo $?
 
     PRINT  copy service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+  echo $?
 
     PRINT  copy mongodb repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+  echo $?
 
     PRINT  adding application user
   useradd roboshop &>>$LOG_FILE
+  echo $?
 
     PRINT  cleaning old content
   rm -rf /app &>>$LOG_FILE
+  echo $?
 
     PRINT  create app directory
   mkdir /app &>>$LOG_FILE
 
     PRINT  download app content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
+  echo $?
   cd /app
 
     PRINT  extract app content
   unzip /tmp/${component}.zip &>>$LOG_FILE
+  echo $?
 
 
     PRINT  install nodejs dependcies
   npm install &>>$LOG_FILE
+  echo $?
+
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
+  echo $?
 }
