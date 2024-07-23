c3d7e002c85a6c836887acde4fc4b49096e6b3ca
renuka <hello@123.com>
2024-07-23 23:39:32 +0530

    keeping code dry

--- redis.sh
+++ redis.sh
@@ -1,7 +1,7 @@
 source common.sh
 component=redis
 
-PRINT Disable redis Default
+PRINT Disbale redis default
 dnf module disable redis -y &>>$LOG_FILE
 STAT $?
 
@@ -9,17 +9,15 @@ PRINT Enable redis 7
 dnf module enable redis:7 -y &>>$LOG_FILE
 STAT $?
 
-
-PRINT install redis 7
+PRINT Install Redis 7
 dnf install redis -y &>>$LOG_FILE
 STAT $?
 
-PRINT update  Redis config file
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected mode/ c protected-mode no' /etc/redis/redis.conf&>>$LOG_FILE
+PRINT Update Redis Config
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 STAT $?
 
-PRINT Start Redsis Service
+PRINT Start Redis Service
 systemctl enable redis &>>$LOG_FILE
 systemctl restart redis &>>$LOG_FILE
 STAT $?
-
