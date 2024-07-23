0bf76a5c873b3448ebe7677390c18f364c6ac91c
renuka <hello@123.com>
2024-07-23 23:27:24 +0530

    keeping code dry

--- common.sh
+++ common.sh
@@ -1,11 +1,11 @@
-LOG_FILE=/tmp/Roboshop.log
+LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 code_dir=$(pwd)
 
 PRINT() {
   echo &>>$LOG_FILE
   echo &>>$LOG_FILE
-  echo "###############################$*###############################" &>>$LOG_FILE
+  echo " ####################################### $* ########################################" &>>$LOG_FILE
   echo $*
 }
 
@@ -15,70 +15,65 @@ STAT() {
   else
     echo -e "\e[31mFAILURE\e[0m"
     echo
-    echo "Refer the log file for the more information: file path: ${LOG_FILE}"
+    echo "Refer the log file for more information : File Path : ${LOG_FILE}"
     exit $1
   fi
-
 }
-APP_PREREQUISTES(){
 
-   PRINT  adding application user
-    id roboshop &>>LOG_FILE
-    if [ $? -ne 0 ]; then
-     useradd roboshop &>>$LOG_FILE
-    fi
-     STAT $?
+APP_PREREQUISTES() {
+  PRINT Adding Application User
+  id roboshop &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG_FILE
+  fi
+  STAT $?
 
-   PRINT Remove old content
-     rm -rf ${app_path}  &>>$LOG_FILE
-     STAT $?
+  PRINT Remove old content
+  rm -rf ${app_path}  &>>$LOG_FILE
+  STAT $?
 
-     PRINT Create App Directory
-     mkdir ${app_path}  &>>$LOG_FILE
-     STAT $?
+  PRINT Create App Directory
+  mkdir ${app_path}  &>>$LOG_FILE
+  STAT $?
 
-     PRINT Download Application Content
-     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip  &>>$LOG_FILE
-     STAT $?
+  PRINT Download Application Content
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip  &>>$LOG_FILE
+  STAT $?
 
-     PRINT Extract Application Content
-     cd ${app_path}
-     unzip /tmp/${component}.zip  &>>$LOG_FILE
-     STAT $?
-
-
-
+  PRINT Extract Application Content
+  cd ${app_path}
+  unzip /tmp/${component}.zip  &>>$LOG_FILE
+  STAT $?
 }
 
 SYSTEMD_SETUP() {
-  PRINT Copy Service file
-  cp ${code_dir}/${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  STAT $?
+    PRINT Copy Service file
+    cp ${code_dir}/${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+    STAT $?
 
-  PRINT Start service
-  systemctl daemon-reload &>>$LOG_FILE
-  systemctl enable ${component} &>>$LOG_FILE
-  systemctl restart ${component} &>>$LOG_FILE
-  STAT $?
+    PRINT Start Service
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable ${component} &>>$LOG_FILE
+    systemctl restart ${component} &>>$LOG_FILE
+    STAT $?
 }
-NODEJS () {
 
-  PRINT disable nodejs default version
+NODEJS() {
+  PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
   STAT $?
 
-  PRINT  enable nojejs 20 module
+  PRINT Enable NodejS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
   STAT $?
 
-
-  PRINT  install nodejs
+  PRINT Install Nodejs
   dnf install nodejs -y &>>$LOG_FILE
   STAT $?
 
   APP_PREREQUISTES
 
-  PRINT  install nodejs dependcies
+  PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
   STAT $?
 
@@ -86,14 +81,17 @@ NODEJS () {
   SYSTEMD_SETUP
 
 }
-JAVA(){
-  PRINT Download Maven and java
+
+
+JAVA() {
+
+  PRINT Install Maven and Java
   dnf install maven -y &>>$LOG_FILE
   STAT $?
 
   APP_PREREQUISTES
 
-  PRINT download Dependencies
+  PRINT Download Dependencies
   mvn clean package &>>$LOG_FILE
   mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
   STAT $?
@@ -101,39 +99,34 @@ JAVA(){
   SCHEMA_SETUP
   SYSTEMD_SETUP
 
-
 }
-SCHEMA_SETUP(){
-  if [ "$schema_setup" == "mongo" ]; then
-    PRINT  copy mongodb repo file
-      cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-      STAT $?
 
-    PRINT install mongodb cilent
+SCHEMA_SETUP() {
+  if [ "$schema_setup" == "mongo" ]; then
+    PRINT COpy MongoDB repo file
+    cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+    STAT $?
+
+    PRINT Install MongoDB Client
     dnf install mongodb-mongosh -y &>>$LOG_FILE
     STAT $?
 
-    PRINT load master data
-    mongosh --host mongo.dev.renuka.online </app/db/master-data.js &>>$LOG_FILE
+    PRINT Load Master Data
+    mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>>$LOG_FILE
     STAT $?
- fi
+  fi
 
-if [ "$sechema_setup" == "mysql" ]; then
-    PRINT install MySQL cilent
-    dnf install mysql-y &>>$LOG_FILE
+  if [ "$schema_setup" == "mysql" ]; then
+    PRINT Install MySQL Client
+    dnf install mysql -y &>>$LOG_FILE
     STAT $?
 
-    PRINT load Schema
-     mysql -h mysql.dev.renuka.online -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-     STAT $?
+    for file in schema master-data app-user; do
+      PRINT Load file - $file.sql
+      mysql -h mysql.dev.rdevopsb80.online -uroot -pRoboShop@1 < /app/db/$file.sql &>>$LOG_FILE
+      STAT $?
+    done
 
-     PRINT load  Mater Schema
-     mysql -h mysql.dev.renuka.online -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-     STAT $?
-
-     PRINT Create app users
-     mysql -h mysql.dev.renuka.online -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-    STAT $?
   fi
 
 }
