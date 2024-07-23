e3d00b8a87870dff15f870ab7027fb34c48b1044
renuka <hello@123.com>
2024-07-23 22:00:22 +0530

    keeping code dry

--- redis.sh
+++ redis.sh
@@ -1,8 +1,22 @@
+PRINT Disable redis Default
 dnf module disable redis -y
+STAT $?
+
+PRINT Enable redis 7
 dnf module enable redis:7 -y
+STAT $?
+
+
+PRINT install redis 7
 dnf install redis -y
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i '/protected mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT update  Redis config file
+sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT Start Redsis Service
 systemctl enable redis
 systemctl restart redis
+STAT $?
 
