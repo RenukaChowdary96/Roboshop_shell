c5ceb1e1da3ca056c8c329fb1a8329c8ace412fc
renuka <hello@123.com>
2024-07-25 21:40:43 +0530

    keeping code dry

--- payment.sh
+++ payment.sh
@@ -1,14 +1,23 @@
-cp payment.service /etc/systemd/system/payment.service
-dnf install python3 gcc python3-devel -y
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-unzip /tmp/payment.zip
-cd /app
-pip3 install -r requirements.txt
+source common.sh
+component=payment
+app_path=/app
 
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+PRINT Copy payment  service file
+cp payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+STAT $?
+
+PRINT install python3
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+STAT $?
+
+APP_PREREQ
+
+PRINT install requirements
+pip3 install -r requirements.txt &>>$LOG_FILE
+STAT $?
+
+PRINT Start payment Service
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable payment &>>$LOG_FILE
+systemctl restart payment &>>$LOG_FILE
+STAT $?
