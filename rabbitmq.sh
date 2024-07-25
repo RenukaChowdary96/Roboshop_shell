0719ebabc0a3f48242bd36bd8dd41d5c06b0b57c
renuka <hello@123.com>
2024-07-25 21:27:55 +0530

    keeping code dry

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,22 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+source common.sh
+component=rabbitmq
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+PRINT Copy rabbitmq repo file
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+STAT $?
+
+PRINT Install rabbitmq
+dnf install rabbitmq-server -y &>>$LOG_FILE
+STAT $?
+
+
+PRINT Start rabbitmq Service
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl restart rabbitmq-server &>>$LOG_FILE
+STAT $?
+
+
+PRINT Adding user and setting permissions
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+STAT $?
